269f86ee5793564af79821d942d8d569461d56c4
Jorge Pirela <jorgehelip@gmail.com>
2019-07-25 23:10:58 -0400

    Less-Related Tasks Done.

--- gulpfile.js
+++ gulpfile.js
@@ -20,6 +20,9 @@ const gulpimagemin = require('gulp-imagemin');
 const ts = require('gulp-typescript');
 const inject = require('gulp-inject');
 const rename = require('gulp-rename');
+const del = require('promised-del');
+const injectString = require('gulp-inject-string');
+const removeEmptyLines = require('gulp-remove-empty-lines');
 
 /* Objects */
 
@@ -101,7 +104,19 @@ const lessMain = `${lessFolder}main.less`;
 
 /* Functions */
 
-
+const cleanMain = () => {
+    return new Promise(function(resolve, reject) {
+        gulp.src(lessMain)
+            .pipe(removeEmptyLines())
+            .on('error', reject)
+            .pipe(gulp.dest(lessFolder))
+            .on('end', resolve)
+    }).then(function() {
+        gutil.log(gutil.colors.blue(`Main Less Line Cleaning Successfull`));
+    }).catch(function(err) {
+        gutil.log(gutil.colors.red(`Error in Main Less Line Cleaning : ${err}`));
+    });
+}
 const beutifyPath = (filePath, filePathLength) => {
     /* 
         Debugging
@@ -128,15 +143,68 @@ const beutifyPath = (filePath, filePathLength) => {
     }
     return beautiPath.join('/');
 }
+const lessKillTheSons = (file) => {
+    let filePath = file;
+    filePath = filePath.split('\\');
+    let filePathLength = filePath.length - 1;
+    let parentFolder = filePath[filePathLength - 1];
+    let slash = `/`;
+    let noErrors = true;
+    if ((parentFolder) == "less") {
+        parentFolder = "";
+        slash = "";
+    }
+    let target = filePath[filePathLength];
+    target = target.split('.');
+    target = target[0];
+    let main = `${cssFolder}${parentFolder}${slash}${target}.css`
+    let map = `${cssFolder}${parentFolder}${slash}${target}.css.map`;
+    let min = `${cssFolder}${parentFolder}${slash}${target}.min.css`;
+    let minMap = `${cssFolder}${parentFolder}${slash}${target}.min.css.map`;
+    del([main, map, min, minMap]).catch(function(err) {
+        noErrors = false;
+        gutil.log(gutil.colors.bgRed(`      Error Deleting The Sons Of Less File : ${err}`));
+    }).then(function() {
+        if (noErrors) {
+            gutil.log(gutil.colors.red(`    Son Of Less File Deleted: ${main}`));
+            gutil.log(gutil.colors.red(`    Son Of Less File Deleted: ${map}`));
+            gutil.log(gutil.colors.red(`    Son Of Less File Deleted: ${min}`));
+            gutil.log(gutil.colors.red(`    Son Of Less File Deleted: ${minMap}`));
+        }
+    });
+    return new Promise(function(resolve, reject) {
+        let filePathP = file;
+        filePathP = filePathP.split('\\');
+        let filePathLengthP = filePathP.length - 1;
+        let parentFolderP = filePathP[filePathLengthP - 1];
+        let slashP = `/`;
+        let noErrors = true;
+        if ((parentFolder) == "less") {
+            parentFolderP = "";
+            slashP = "";
+        }
+        let targetP = filePathP[filePathLengthP];
+        targetP = targetP.split('.');
+        targetP = targetP[0];
+        gulp.src(lessMain)
+            .pipe(injectString.replace(`@import "${parentFolderP}${slashP}${targetP}.less";`, ``))
+            .on('error', reject)
+            .pipe(gulp.dest(`${lessFolder}`))
+            .on('end', resolve);
+    }).then(function() {
+        gutil.log(gutil.colors.red(`    Less File Deleted From Main.less`));
+        cleanMain();
+    }).catch(function(err) {
+        gutil.log(gutil.colors.bgRed(`Error: ${filePath}`));
+    });
+}
 const lessifyAll = () => {
-    let target = gulp.src(lessFiles);
+    let target = gulp.src(lessFiles, { base: lessFolder });
     return new Promise(function(resolve, reject) {
         target
             .pipe(sourcemaps.init())
             .on('error', reject)
-            .pipe(less({
-                paths: [lessFiles],
-            }))
+            .pipe(less())
             .on('error', reject)
             .pipe(prefix({
                 browsers: ['last 99 versions']
@@ -149,7 +217,9 @@ const lessifyAll = () => {
                 }
             }))
             .on('error', reject)
-            .pipe(rename({ extname: '.min.css' }))
+            .pipe(rename(function(path) {
+                path.basename = `${path.basename}.min`;
+            }))
             .on('error', reject)
             .pipe(sourcemaps.write('./'))
             .on('error', reject)
@@ -157,7 +227,7 @@ const lessifyAll = () => {
             .on('end', resolve)
     }).then(function() {
         // Resolve
-        gutil.log(gutil.colors.green(`All Less Files Have Been Compiled`));
+        gutil.log(gutil.colors.green(`Less Files Compiled Without Error`));
     }).catch(function(err) {
         gutil.log(gutil.colors.red(`Error: ${err}`));
     });
@@ -167,30 +237,34 @@ const lessifyOne = (file) => {
     let filePath = file.split('\\');;
     let filePathLength = filePath.length - 1;
     let pathToPrint = beutifyPath(filePath, filePathLength);
+    let parentFolder = filePath[filePathLength - 1];
+    if ((parentFolder) == "less") {
+        parentFolder = "";
+    }
     return new Promise(function(resolve, reject) {
         target
             .pipe(sourcemaps.init())
             .on('error', reject)
-            .pipe(less({
-                paths: [file],
-            }))
+            .pipe(less())
             .on('error', reject)
             .pipe(prefix({
                 browsers: ['last 99 versions']
             }))
             .on('error', reject)
-            .pipe(gulp.dest(cssFolder))
+            .pipe(gulp.dest(`${cssFolder}${parentFolder}`))
             .pipe(minifyCSS({
                 level: {
                     1: minifyLevelOption
                 }
             }))
             .on('error', reject)
-            .pipe(rename({ extname: '.min.css' }))
+            .pipe(rename(function(path) {
+                path.basename = `${path.basename}.min`;
+            }))
             .on('error', reject)
             .pipe(sourcemaps.write('./'))
             .on('error', reject)
-            .pipe(gulp.dest(cssFolder))
+            .pipe(gulp.dest(`${cssFolder}${parentFolder}`))
             .on('end', resolve)
     }).then(function() {
         // Resolve
@@ -335,7 +409,7 @@ const lessInjectOne = (file) => {
         function() {
             // Resolved
             gutil.log(gutil.colors.green(`A New '${type}' Less File Have Been Injected: ./${pathToPrint}`));
-            lessifyOne(file);
+            lessifyAll()
         }).catch(function(err) {
         gutil.log(gutil.colors.red(`An Error Occured While Injecting New '${type}' Less File : ./${pathToPrint}`));
         gutil.log(gutil.colors.red(`Error: ${err}`));
@@ -345,17 +419,32 @@ const lessInjectOne = (file) => {
 /* Tasks */
 
 gulp.task('default', function(done) {
-    console.log('Here');
+    cleanMain();
     done();
 });
 
-gulp.task('inject', function(done) {
+gulp.task('less', function(done) {
     lessInjectAll();
     done();
 });
-gulp.task('watch-inject', function(done) {
+gulp.task('watch-less', function(done) {
     gulp.src(lessFiles)
         .pipe(watch(lessFiles)).on('add', function(filePath) {
+            let dividedPath = filePath.split('\\');
+            let toPrint = beutifyPath(dividedPath, (dividedPath.length - 1));
+            gutil.log(gutil.colors.green(`Less File Added : ${toPrint}`));
             lessInjectOne(filePath);
-        });
+        })
+        .pipe(watch(lessFiles)).on('change', function(filePath) {
+            let dividedPath = filePath.split('\\');
+            let toPrint = beutifyPath(dividedPath, (dividedPath.length - 1));
+            gutil.log(gutil.colors.yellow(`Less File Changed : ${toPrint}`));
+            lessifyAll();
+        })
+        .pipe(watch(lessFiles)).on('unlink', function(filePath) {
+            let dividedPath = filePath.split('\\');
+            let toPrint = beutifyPath(dividedPath, (dividedPath.length - 1));
+            gutil.log(gutil.colors.red(`Less File Deleted : ${toPrint}`));
+            lessKillTheSons(filePath);
+        })
 });
